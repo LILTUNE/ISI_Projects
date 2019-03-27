c69333fa49287f5d3e99fb0766887dd8ce8e6021
Tong Liu <liu553@usc.edu>
2019-03-27 10:50:18 -0700

    Add files via upload

--- sparql.py
+++ sparql.py
@@ -1,27 +1,33 @@
 from SPARQLWrapper import SPARQLWrapper, JSON,XML
+import re
+from collections import defaultdict
 
-def get_properties(P_ID):
+def get_properties(P_ID, filename):
 	sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
+	sparql.setTimeout(400)
 	sparql.setQuery("""
 	    SELECT DISTINCT ?v
 		WHERE{
 	      ?x wdt:""" + P_ID + """?v .
 	      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 	    }
-	    LIMIT 10
 	""")
 	sparql.setReturnFormat(JSON)
-	results = sparql.query().convert()
-	res = results["results"]["bindings"]
+	try:
+		results = sparql.query().convert()
+		res = results["results"]["bindings"]
 	#results["results"]["bindings"][0]["v"]["value"]
-	for r in res:
-		print(r["v"]["value"])
+		f = open("./results/"+filename,"w+")
+		for r in res:
+			f.write((r["v"]["value"]))
+			f.write("\n")
+		f.close()
+	except:
+		print(filename)
 
-def write_file():
-	continue
-	##todo
 
 def get_identifiers():
+	global id_label
 	sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
 	sparql.setQuery("""
 		SELECT DISTINCT ?P_ID ?P_IDLabel
@@ -32,5 +38,24 @@ def get_identifiers():
 		}
 		ORDER BY DESC(?P_ID)
 	""")
+	sparql.setReturnFormat(JSON)
+	try:
+		results = sparql.query().convert()
+		res = results["results"]["bindings"]
+		for r in res:
+			try:
+				id = re.search(r'[A-Za-z][0-9]+',r["P_ID"]["value"]).group()
+				label = r["P_IDLabel"]["value"]
+				id_label[id]=label
+			except:
+				continue
+		#print(re.search(r'[PQ][0-9]+', a).group())
+	except:
+		print(error)
 if __name__ == "__main__":
+	id_label = defaultdict()
 	#get_properties("P212")
+	get_identifiers()
+	for id,label in id_label.items():
+		#print("-"*20)
+		get_properties(id,id+"_"+label+".txt")
