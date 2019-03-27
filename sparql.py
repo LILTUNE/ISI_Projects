dc5f3e42d6dc21709e0c2c1d3f2b56fb3490109c
Tong Liu <liu553@usc.edu>
2019-03-27 14:25:29 -0700

    Update sparql.py

--- sparql.py
+++ sparql.py
@@ -3,8 +3,9 @@ import re
 from collections import defaultdict
 
 def get_properties(P_ID, filename):
-	sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
-	sparql.setTimeout(400)
+	sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+	#sparql = SPARQLWrapper("http://sitaware.isi.edu:8080")
+	sparql.setTimeout(1000)
 	sparql.setQuery("""
 	    SELECT DISTINCT ?v
 		WHERE{
@@ -16,8 +17,7 @@ def get_properties(P_ID, filename):
 	try:
 		results = sparql.query().convert()
 		res = results["results"]["bindings"]
-	#results["results"]["bindings"][0]["v"]["value"]
-		f = open("./results/"+filename,"w+")
+		f = open("./results_2.0/"+filename,"w+")
 		for r in res:
 			f.write((r["v"]["value"]))
 			f.write("\n")
@@ -28,7 +28,9 @@ def get_properties(P_ID, filename):
 
 def get_identifiers():
 	global id_label
-	sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
+	sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+	#sparql = SPARQLWrapper("http://sitaware.isi.edu:8080")
+	sparql.setTimeout(1000)
 	sparql.setQuery("""
 		SELECT DISTINCT ?P_ID ?P_IDLabel
 		WHERE
@@ -39,23 +41,17 @@ def get_identifiers():
 		ORDER BY DESC(?P_ID)
 	""")
 	sparql.setReturnFormat(JSON)
-	try:
-		results = sparql.query().convert()
-		res = results["results"]["bindings"]
-		for r in res:
-			try:
-				id = re.search(r'[A-Za-z][0-9]+',r["P_ID"]["value"]).group()
-				label = r["P_IDLabel"]["value"]
-				id_label[id]=label
-			except:
-				continue
-		#print(re.search(r'[PQ][0-9]+', a).group())
-	except:
-		print(error)
+	results = sparql.query().convert()
+	res = results["results"]["bindings"]
+	for r in res:
+		try:
+			id = re.search(r'[A-Za-z][0-9]+',r["P_ID"]["value"]).group()
+			label = r["P_IDLabel"]["value"]
+			id_label[id]=label
+		except:
+			continue	
 if __name__ == "__main__":
-	id_label = defaultdict()
-	#get_properties("P212")
+	id_label = defaultdict()#get_properties("P212")
 	get_identifiers()
 	for id,label in id_label.items():
-		#print("-"*20)
 		get_properties(id,id+"_"+label+".txt")
