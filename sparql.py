b239914792cad5881fcce6bfc8090c2b0f188b23
Tong Liu <liu553@usc.edu>
2019-04-21 10:36:34 -0700

    changed sparql inside get_identifiers() function

--- sparql.py
+++ sparql.py
@@ -1,7 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON,XML
 import re,json
 
-def get_properties(P_ID, filename):
+def get_properties(P_ID):
 	#sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 	cur_json = {}
 	sparql = SPARQLWrapper("http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql")
@@ -22,41 +22,38 @@ def get_properties(P_ID, filename):
 			if Q:
 				cur_json[id] = Q.group(0)
 	except:
-		print(filename)
+		print(P_ID)
 	return cur_json
 
 
 def get_identifiers():
-	global id_label
+	global P_nodes
 	#sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 	sparql = SPARQLWrapper("http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql")
 	#sparql.setTimeout(1000)
-	sparql.setQuery("""
-		SELECT DISTINCT ?P_ID ?P_IDLabel
-		WHERE
-		{
-		  ?P_ID wdt:P31/wdt:P279* wd:Q19847637 .
-		  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-		}
-		ORDER BY DESC(?P_ID)
-	""")
+	sparql.setQuery("""select ?p where {
+		?p wikibase:propertyType wikibase:ExternalId .
+		}""")
 	sparql.setReturnFormat(JSON)
 	results = sparql.query().convert()
 	res = results["results"]["bindings"]
 	for r in res:
 		try:
-			id = re.search(r'[A-Za-z][0-9]+',r["P_ID"]["value"]).group()
-			label = r["P_IDLabel"]["value"]
-			id_label[id]=label
+			P_node = re.search(r'[A-Za-z][0-9]+',r["p"]["value"]).group()
+			P_nodes.append(P_node)
+			# label = r["P_IDLabel"]["value"]
+			# id_label[id]=label
 		except:
 			continue
 if __name__ == "__main__":
 	id_label = {}#get_properties("P212")
+	P_nodes = []
 	prop_idents ={}
 	get_identifiers()
-	for id,label in id_label.items():
-		prop_idents[id+"_"+label] = get_properties(id,id+"_"+label)
+	for idx,P_node in enumerate(P_nodes):
+		print(idx,P_node)
+		prop_idents[P_node] = get_properties(P_node)
 	js = json.dumps(prop_idents)
-	f = open("prop_idents.json","w+")
+	f = open("prop_idents_v2.json","w+")
 	f.write(js)
 	f.close()
