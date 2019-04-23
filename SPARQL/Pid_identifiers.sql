9e9fed8f04bd57cbc07b8ea2e426c0b73cab46db
Tong Liu <liu553@usc.edu>
2019-04-23 16:13:44 -0700

    Update Pid_identifiers.sql

--- SPARQL/Pid_identifiers.sql
+++ SPARQL/Pid_identifiers.sql
@@ -12,3 +12,15 @@ WHERE
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 ORDER BY DESC(?P_ID)
+
+//select Qnodes and Identifiers for some property(P288)
+SELECT ?x ?v WHERE { ?x wdt:P232 ?v. }
+
+//select all properties.
+select ?p where {
+		?p wikibase:propertyType wikibase:ExternalId .
+		}
+    //official endpoint
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    //siteware endpoint
+sparql = SPARQLWrapper("http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql")
