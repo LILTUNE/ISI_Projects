14548f8b8b2e32d9faf90e2188919179df266976
Tong Liu <liu553@usc.edu>
2019-04-30 14:29:35 -0700

    Update convert_dict.py

--- convert_dict.py
+++ convert_dict.py
@@ -9,10 +9,10 @@ def convert_dict(prop_idents):
         for identi,Q_number in p_entity.items():
             #if identi in identifier_nodes_dict.keys():
             if identi in keys:
-                identifier_nodes_dict[identi].add(P_number + '/' + Q_number)
+                identifier_nodes_dict[identi].append(P_number + '/' + Q_number)
             else:
                 keys.add(identi)
-                identifier_nodes_dict[identi] = set([P_number + '/' + Q_number])
+                identifier_nodes_dict[identi] = [P_number + '/' + Q_number]
     return identifier_nodes_dict
 
 def dict_slice(adict, start, end):
